f21cc2d0d40df5880a078e3c3a2c92e825dfc1bc
Mark Otweyo <markotweyo99@gmail.com>
2021-10-05 10:21:24 +0300

    Adding Sign up/Sign in form and client-side auth

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,17 @@ import React from 'react'
 import {StreamChat} from 'stream-chat'
 import {Chat} from 'stream-chat-react'
 import Cookies from 'universal-cookie'
-import {ChannelContainer, ChannelListContainer} from './components'
+import {ChannelContainer, ChannelListContainer, Auth} from './components'
 import './App.css'
 
 const apiKey ='h72rd5zh39z6'
 
 const client= StreamChat.getInstance(apiKey)
 
+const authToken= false
 const App = () => {
+
+  if(!authToken) return <Auth/>
   return (
     <div className='app__wrapper'>
       <Chat client={client} theme= 'team light'>
